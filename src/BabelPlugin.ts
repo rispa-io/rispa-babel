47b7532b4cf6f82a49ae403d575c3707615ff68c
dvpetrov <dvpetrov.main@gmail.com>
2017-10-08 12:41:32 +0300

    Throw invalid types

--- src/BabelPlugin.ts
+++ src/BabelPlugin.ts
@@ -1,27 +1,44 @@
-import { PluginInstance } from '@rispa/core'
+import { PluginInstance, RispaConfig } from '@rispa/core'
 import { TransformOptions } from 'babel-core'
 import mergeBabelConfigs = require('babel-merge')
 import defaultBabelConfig from './babelConfig'
 
+export type BabelConfigFabric = (config: RispaConfig) => TransformOptions
+export type BabelConfig = TransformOptions | BabelConfigFabric
+
 class BabelPlugin extends PluginInstance {
-  private babelConfig: TransformOptions[] = []
+  private babelConfig: BabelConfig[] = []
 
   start() {
     this.addConfig(defaultBabelConfig)
   }
 
-  addConfig(...config: TransformOptions[]) {
-    this.babelConfig = this.babelConfig.concat(config)
+  addConfig(...configs: BabelConfig[]) {
+    configs.forEach(config => {
+      const configType = typeof config
+
+      if (configType !== 'function' && configType !== 'object') {
+        throw new TypeError('Invalid babel config type')
+      }
+    })
+
+    this.babelConfig = this.babelConfig.concat(configs)
   }
 
   getConfig(): TransformOptions {
-    return this.babelConfig.reduce((result, config) => {
+    if (this.babelConfig.length === 0) {
+      throw new Error('Empty babel config')
+    }
+
+    const babelConfig = this.babelConfig.reduce((result, config) => {
       if (typeof config === 'function') {
-        return mergeBabelConfigs(result, config())
+        return mergeBabelConfigs(result, config(this.config))
       }
 
       return mergeBabelConfigs(result, config)
     }, {})
+
+    return babelConfig
   }
 }
 
