2b2861839aefa524780a427c95521f552d0c8959
dvpetrov <dvpetrov.main@gmail.com>
2017-10-09 15:13:53 +0300

    Remove `add module exports` babel plugin

--- src/babelConfig.ts
+++ src/babelConfig.ts
@@ -4,7 +4,7 @@ import * as path from 'path'
 const config: TransformOptions = {
   env: {
     production: {
-      plugins: [
+      plugins: [ // TODO: Move to React plugin
         require.resolve('babel-plugin-transform-react-constant-elements'),
         require.resolve('babel-plugin-transform-react-inline-elements'),
         require.resolve('babel-plugin-transform-react-remove-prop-types'),
@@ -18,7 +18,7 @@ const config: TransformOptions = {
   },
 
   presets: [
-    require.resolve('babel-preset-react'),
+    require.resolve('babel-preset-react'), // TODO: Move to React plugin
     require.resolve('babel-preset-es2015'),
     require.resolve('babel-preset-es2016'),
     require.resolve('babel-preset-es2017'),
@@ -28,13 +28,13 @@ const config: TransformOptions = {
     [require.resolve('babel-plugin-transform-runtime'), {
       moduleName: path.resolve(__dirname, '../node_modules/babel-runtime'),
     }],
-    require.resolve('babel-plugin-add-module-exports'),
+    // require.resolve('babel-plugin-add-module-exports'),
     require.resolve('babel-plugin-transform-decorators-legacy'),
     require.resolve('babel-plugin-transform-react-display-name'),
     require.resolve('babel-plugin-transform-object-rest-spread'),
     require.resolve('babel-plugin-transform-class-properties'),
     require.resolve('babel-plugin-dynamic-import-webpack'),
-    [require.resolve('babel-plugin-transform-imports'), {
+    [require.resolve('babel-plugin-transform-imports'), { // TODO: Move settings to BabelPluginApi
       ramda: {
         transform: 'ramda/src/${member}',
         preventFullImport: true,
